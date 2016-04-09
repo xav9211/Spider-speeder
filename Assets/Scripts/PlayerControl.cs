17e8d907510e38b2605c8d5cace891ec2512c451
Kamil Kandefer <kamil.0392@gmail.com>
2016-04-09 19:19:14 +0200

    Add simple enemy

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -63,6 +63,11 @@ public class PlayerControl: MonoBehaviour {
     void Update() {
         Move();
     }
+    
+    void Die()
+    {
+        Destroy(this.gameObject);
+    }
 
     void Move() {
         WebControl(KeyCode.Joystick1Button4, legs[Legs.TopLeft]);
@@ -103,6 +108,10 @@ public class PlayerControl: MonoBehaviour {
                 Vector2 pointingDirection = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
                 LayerMask layerMask = ~(1 << LayerMask.NameToLayer("Player"));
                 RaycastHit2D hitInfo = Physics2D.Raycast(legTransform.position, pointingDirection, swingRange, layerMask);
+                if (hitInfo.collider.name == "Enemy")
+                {
+                    hitInfo.rigidbody.SendMessage("Die");
+                }
                 if (hitInfo.collider) {
                     SpringJoint2D spring = legObject.AddComponent<SpringJoint2D>();
                     spring.anchor.Set(0, 1);
