d5f01e493757105cddaf2dc04237abf9418f1d75
Marcin Radomski <marcin@mradomski.pl>
2016-05-19 17:44:04 +0200

    Reduce immunity time to 1s

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -265,7 +265,7 @@ public class PlayerControl: MonoBehaviour, DamageSource {
 
         Health -= damage;
 
-        const float IMMUNITY_TIME_S = 3.0f;
+        const float IMMUNITY_TIME_S = 1.0f;
         IsImmune = true;
         Invoke("StopImmunity", IMMUNITY_TIME_S);
 
