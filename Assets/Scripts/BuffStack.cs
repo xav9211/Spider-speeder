6793d0e942a5e991383b1e9512cf222562feadf5
Marcin Radomski <marcin@mradomski.pl>
2016-05-28 00:41:24 +0200

    Add buffs bar

--- Assets/Scripts/BuffStack.cs
+++ Assets/Scripts/BuffStack.cs
@@ -9,20 +9,38 @@ namespace Assets.Scripts {
         Damage,
     }
 
-    public struct Buff {
+    public class Buff {
         public enum Type {
             Additive,
             Multiplicative
         }
 
-        public float timeout;
-        public Type type;
         public Statistic stat;
+        public Type type;
         public float value;
+        public float timeout;
+        public string icon;
+
+        public Buff(Statistic stat,
+                    Type type,
+                    float value,
+                    float timeout,
+                    string icon) {
+            this.stat = stat;
+            this.type = type;
+            this.value = value;
+            this.timeout = timeout;
+            this.icon = icon;
+        }
     }
 
     public class BuffStack: MonoBehaviour {
         public Dictionary<Statistic, Dictionary<Buff.Type, List<Buff>>> Buffs { get; private set; }
+        private BuffStackDisplay display;
+
+        void Start() {
+            display = GameObject.FindGameObjectWithTag("BuffStackDisplay").GetComponent<BuffStackDisplay>();
+        }
 
         public BuffStack() {
             Buffs = new Dictionary<Statistic, Dictionary<Buff.Type, List<Buff>>>();
@@ -36,15 +54,29 @@ namespace Assets.Scripts {
             }
         }
 
+        private List<Buff> AllBuffs() {
+            List<Buff> allBuffs = new List<Buff>();
+
+            foreach (var perStatBuffs in Buffs.Values) {
+                foreach (var perStatPerTypeBuffs in perStatBuffs.Values) {
+                    allBuffs.AddRange(perStatPerTypeBuffs);
+                }
+            }
+
+            return allBuffs;
+        }
+
         private IEnumerator RemoveWhenExpired(Buff buff) {
             yield return new WaitForSeconds(buff.timeout);
             //print("destroying buff: " + buff.stat + ", " + buff.type + ", " + buff.value + " for " + buff.timeout);
             Buffs[buff.stat][buff.type].Remove(buff);
+            display.Refresh(AllBuffs());
         }
 
         public void Add(Buff buff) {
             //print("adding buff: " + buff.stat + ", " + buff.type + ", " + buff.value + " for " + buff.timeout);
             Buffs[buff.stat][buff.type].Add(buff);
+            display.Refresh(AllBuffs());
 
             // weird way to schedule something to be done after a delay
             // http://answers.unity3d.com/questions/897095/workaround-for-using-invoke-for-methods-with-param.html
