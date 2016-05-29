1340d599b9942a9efb83510d90b4d1147567d95d
Marcin Radomski <marcin@mradomski.pl>
2016-05-30 00:02:33 +0200

    Main menu, without highscores

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+    void StartNewGame() {
+        SceneManager.LoadScene("Game");
+    }
+
+    void ShowHighScores() {
+        
+    }
+
+    void Exit() {
+        Application.Quit();
+    }
+}
